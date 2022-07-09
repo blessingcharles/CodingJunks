efde3176dc8c5f94092c754a2b88444f8264cfa0
blessingcharles <charlesblessing001@gmail.com>
2022-07-09 12:51:42 +0530

    aa

--- codingstudio/theirprep/multidimensional.md
+++ codingstudio/theirprep/multidimensional.md
@@ -11,4 +11,93 @@ bool isMatrixSymmetric(vector<vector<int>> matrix){
 }
 ```
 
-2. 
+2. [search in sorted matrix]()
+```cpp
+pair<int, int> search(vector<vector<int>> matrix, int x)
+{
+    // Write your code here.
+    for(int i = 0 ; i < matrix.size() ; i++){
+            int left = 0 , right = matrix[0].size()-1 , mid ;
+            while(left <= right){
+                mid = left + (right - left)/2 ;
+                if(matrix[i][mid] == x) return {i , mid};
+                else if(matrix[i][mid] > x) right = mid-1 ;
+                else left = mid+1 ;
+            }
+   }
+   return {-1,-1};
+}
+```
+
+3. [Right rotate matrix](https://www.codingninjas.com/codestudio/guided-paths/interview-guide-for-product-based-companies/content/111356/offering/1280161?leftPanelTab=0)
+```cpp
+vector<int> rotateMatRight(vector<vector<int>> mat, int n, int m, int k) {
+    int M = mat.size() , N = mat[0].size() , start_col ;
+    k = k%N ;
+    if(k == 0){
+        start_col = 0;
+    }
+    else{
+         start_col = N-k ;   
+    }
+    vector<int> res ;
+    int r_col ;
+    
+    for(int row = 0 ;row < M ; row++){
+        
+        for(int i = 0 ; i < N ; i++){
+            r_col = (start_col + i)%N ;
+            res.push_back(mat[row][r_col]);
+        }
+    }
+    return res ;
+}
+```
+
+4.  [count flips](https://www.codingninjas.com/codestudio/guided-paths/interview-guide-for-product-based-companies/content/111356/offering/1280162?leftPanelTab=3)
+```cpp
+int countFlip(vector<vector<int>> &mat)
+{
+	bool first_col  = false ;
+    int M = mat.size() , N = mat[0].size() ;
+    int flip = 0 ;
+    
+    for(int row = 0 ; row < M ; row++){
+        for(int col = 0 ; col < N ; col++){
+            if(mat[row][col] == 0){
+                if(col == 0){
+                    first_col = true ;
+                    continue ;
+                }
+                
+                if(mat[row][0] == 1) flip++ ;
+                if(mat[0][col] == 1) flip++ ;
+                
+                mat[row][0] = 0 ;
+                mat[0][col] = 0 ;
+            }
+        }
+    }
+    
+    for(int row = N-1 ; row >= 0 ; row--){
+        for(int col = N-1 ; col >= 0 ; col--){
+            if(mat[row][col] == 1){
+                if(col == 0){
+                    if(first_col){
+                        flip++ ;
+                        mat[row][col] = 0 ;
+                    }
+                }
+                else{
+                    if(mat[0][col] == 0 or mat[row][0] == 0){
+                        flip++ ;
+                        mat[row][col] = 0 ;
+                    }
+                }
+            }
+        }
+    }
+    return flip ;
+    
+}
+```
--- test.cpp
+++ test.cpp
@@ -5,73 +5,32 @@ using namespace std ;
 
 #include<bits/stdc++.h>
 
-vector<int> shortestAlternateColoredPath(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) 
+string shortestSubstring(string s)
 {
-    vector<bool> selfloops(n , false);
+    unordered_set<char> myset(s.begin() , s.end());
+    int n = myset.size() ;
+    int left = 0 , right = 0 ;
+    unordered_map<char , int> memo ; 
+    int left_idx = 0 ,right_idx , maxlen = s.size() ;
     
-    vector<vector<pair<int,int>>> graph(n);
-    for(int i = 0 ; i < redEdges.size() ; i++){
-        if(redEdges[i][0] == redEdges[i][1]){
-            selfloops[i] = true ;
-        }
-        graph[redEdges[i][0]].push_back({redEdges[i][1] ,1});
-    }
-    for(int i = 0 ; i < blueEdges.size() ; i++){
-        if(blueEdges[i][0] == blueEdges[i][1]){
-            selfloops[i] = true ;
-        }
-        graph[blueEdges[i][0]].push_back({blueEdges[i][1] ,2}) ;
-    }
-    vector<int> visited(n , -1);
-    int curdist = 0 ;
-    queue<pair<int,int>> q ; // {node , color}
-    q.push({0,0});
-    pair<int,int> curnode ;
-    
-    while(not q.empty()){
-        int sz = q.size() ;
-
-        while(sz--){
-            curnode = q.front() ; q.pop() ;
-            cout << curnode.first << " with color " << curnode.second << endl ;
-            bool flag = true ;
-            // special previlege
-            if(visited[curnode.first] != -1 and selfloops[curnode.first]){
-                cout << "[+] Settingprevilege \n";
-
-                flag = false ;
-                selfloops[curnode.first] = false ;
+    while(right < n){
+        memo[s[right]]++ ;
+        while(memo.size() == n){
+            int curlen  = right-left+1 ;
+            if(curlen < maxlen){
+                left_idx = left ;
+                maxlen = curlen ;
             }
-            
-
-            for(auto neigh : graph[curnode.first]){
-                cout << "visiting " << neigh.first << "color " << neigh.second << endl ;
-                // already visited
-                if(flag and visited[neigh.first] != -1) continue ;
-                //same color
-                if(flag and curnode.second == neigh.second) continue ;
-                
-  
-                cout << "pushing it" << endl ; 
-                q.push(neigh);
-            }
-            if(visited[curnode.first] == -1)
-                    visited[curnode.first] = curdist ;
-            cout << "----------------\n" ;
+            memo[s[left]]-- ;
+            if(memo[s[left]] == 0) memo.erase(memo[s[left]]);
+            left++ ;
         }
-        curdist++ ;
-
+        right++ ;
     }
-    visited[0] = 0 ;
-    return visited ;
+    return s.substr(left_idx ,maxlen);
 }
+
 int main()
 {
-    vector<vector<int>> red  = {{0,1},{1,3}};
-    vector<vector<int>> blue = {{1,1},{1,2}};
-
-    for(auto ele : shortestAlternateColoredPath(4 , red , blue )){
-        cout << ele <<  " " << endl ;
-    }
-    return 0 ;
+    cout << shortestSubstring("abbbcbbbbabbc");
 }
