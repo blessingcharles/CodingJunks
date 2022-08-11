2d8bf61bf39f51e7136d1d1b788298b6a82a89e7
blessingcharles <charlesblessing001@gmail.com>
2022-08-11 20:09:00 +0530

    something

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 #include<bits/stdc++.h>
 
 using namespace std ;
@@ -6,13 +5,11 @@ using namespace std ;
 
 int main(){
 
-    vector<vector<int>> arr = {{1,2} , {1,5} , {3,5} , {5,9} ,{5,3} , {8,9}} ;
-
-    auto it = *lower_bound(arr.begin() , arr.end() , 4 , [](const vector<int> &b , int val){
-        return b[0] >= val ;
-    }) ;
-
-    // for(int ele : it) cout << ele << " " ;
-
-    // cout << d->name << endl ;
+    char *p[] = {"aaa" , "bbb" , "cccc" , "dddd"} ;
+    char **ptr[]  = {p+3 , p+2 , p+1 , p} ;
+    char ***pptr = ptr ;
+    
+    printf("%p ------ [%p %p %p %p]\n" , &p , *p , *(p+1) , *(p+2), *(p+3)) ;
+    printf("%p ----- [%p]" , &ptr , **(ptr));
+    return 0 ;
 }
